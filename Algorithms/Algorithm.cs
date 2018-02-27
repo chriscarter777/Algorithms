71c86a10b72a4850dc7ccf5857fca44be36602b7
Christopher Carter <christopher.carter.777@gmail.com>
2018-02-26 21:39:55 -0800

    initial

---
+++ Algorithms/Algorithm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+     public static class Algorithm
+     {
+          public static string ReverseString0(string original)
+          {
+               char[] originalArray = original.ToCharArray();
+               Array.Reverse(originalArray);
+               return new string(originalArray);
+          }
+
+          public static string ReverseString1(string original)
+          {
+               char[] originalArray = original.ToCharArray();
+               int size = originalArray.Length;
+               char[] reversedArray = new char[size];
+               for (int i = 0, j = (size - 1); i < size; i++, j--)
+               {
+                    reversedArray[j] = originalArray[i];
+               }
+               return new string(reversedArray);
+          }
+
+          public static string ReverseString2(string original)
+          {
+               char[] origArray = original.ToCharArray();
+               for (int i = 0, j = (origArray.Length - 1); i < j; i++, j--)
+               {
+                    char c = origArray[i];
+                    origArray[i] = origArray[j];
+                    origArray[j] = c;
+               }
+               return new string(origArray);
+          }
+
+          public static int Factorial(int i)
+          {
+               if(i == 1)
+               {
+                    return 1;
+               }
+               return i * Factorial(i - 1);
+          }
+
+          public static int FactorialNonRecur(int operand)
+          {
+               int result = 1;
+               for (int i = 2; i <= operand; i++)
+               {
+                    result = result * i;
+               }
+               return result;
+          }
+
+          public static void TraverseBTInOrder(BinaryNode<string> node, List<string> result)
+          {
+               if(node != null)
+               {
+                    if(node.Left != null)
+                    {
+                         TraverseBTInOrder(node.Left, result);
+                    }
+                    result.Add(node.Value);
+                    if (node.Right != null)
+                    {
+                         TraverseBTInOrder(node.Right, result);
+                    }
+               }
+          }
+
+          public static void TraverseBTPreOrder(BinaryNode<string> node, List<string> result)
+          {
+               if (node != null)
+               {
+                    result.Add(node.Value);
+                    if (node.Left != null)
+                    {
+                         TraverseBTPreOrder(node.Left, result);
+                    }
+                    if (node.Right != null)
+                    {
+                         TraverseBTPreOrder(node.Right, result);
+                    }
+               }
+
+          }
+
+          public static void TraverseBTPostOrder(BinaryNode<string> node, List<string> result)
+          {
+               if (node != null)
+               {
+                    if (node.Left != null)
+                    {
+                         TraverseBTPostOrder(node.Left, result);
+                    }
+                    if (node.Right != null)
+                    {
+                         TraverseBTPostOrder(node.Right, result);
+                    }
+                    result.Add(node.Value);
+               }
+          }
+
+          public static string BalanceScale(string[] input)
+          {
+               //Read string array which will contain two elements, 
+               //the first being the two positive integer weights on a balance scale (left and right), and 
+               //the second element being a list of available weights as positive integers. 
+               //Your goal is to determine if you can balance the scale by using the least amount of weights from the list, at most 2 weights.
+               //Your program should return a comma-separated string of the weights that were used from the list in ascending order.
+
+               //There will only ever be one unique solution and the list of available weights will not be empty. 
+               //Add weights to only one side of the scale to balance it. 
+               //If it is not possible to balance the scale, return the string "not possible".
+
+               //parse input string
+               string[] givens = input[0].Replace('[',' ').Replace(']', ' ').Split(',');
+               int left = Int32.Parse(givens[0].Trim());
+               int right = Int32.Parse(givens[1].Trim());
+               int diff = Math.Abs(left - right);
+
+               string[] usewts = input[1].Replace('[', ' ').Replace(']', ' ').Split(',');
+               int[] options = new int[usewts.Length];
+               for(int i = 0; i < usewts.Length; i++)
+               {
+                    options[i] =  Int32.Parse(usewts[i].Trim());
+               }
+               Array.Sort(options);
+
+               //can you do it with one weight?
+               for( int i = 0; i < options.Length; i++ )
+               {
+                    if(options[i] == diff)
+                    {
+                         return options[i].ToString();
+                    }
+               }
+
+               //can you do it with two weights?
+               for (int i = 1; i < options.Length; i++)
+               {
+                    for (int j = 0; j < i; j++)
+                    {
+                         if (options[i] + options[j] == diff)
+                         {
+                              //return ascending (options is sorted Asc)
+                              return options[j].ToString() + "," + options[i].ToString();
+                         }
+                    }
+               }
+               return "not possible";
+          }
+
+          public static bool QuestionMark(string s)
+          {
+               // Take an input string parameter and determine if exactly 3 question marks exist between every pair of numbers that add up to 10.
+               // If so, return true, otherwise return false.
+               char[] chars = s.ToCharArray();
+               for (int i = 0; i < chars.Length - 1; i++)
+               {
+                    if (Char.IsNumber(chars[i]))
+                    {
+                         int counter = 0;
+                         for (int j = i + 1; j < chars.Length; j++)
+                         {
+                              if(chars[j] == '?')
+                              {
+                                   counter++;
+                              }
+                              if (Char.IsNumber(chars[j]))
+                              {
+                                   if (Char.GetNumericValue(chars[i]) + Char.GetNumericValue(chars[j]) == 10){
+                                        if(counter != 3)
+                                        {
+                                             return false;
+                                        }
+                                   }
+                                   else
+                                   {
+                                        break;
+                                   }
+                              }
+
+                         }
+                    }
+               }
+               return true;
+          }
+     }  //class
+
+     public class BinaryNode<T>
+     {
+          public T Value { get; set; }
+          public BinaryNode<T> Left { get; set; }
+          public BinaryNode<T> Right { get; set; }
+
+          public BinaryNode(T val)
+          {
+               Value = val;
+          }
+
+
+          public void AddLeftNode(BinaryNode<T> newNode)
+          {
+               Left = newNode;
+          }
+
+          public void AddRightNode(BinaryNode<T> newNode)
+          {
+               Right = newNode;
+          }
+     }  //class
+}  //namespace
---
+++ AlgorithmsTests/AlgorithmTests.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AlgorithmsTests
+{
+     [TestClass]
+     public class AlgorithmTests
+     {
+          [TestMethod]
+          public void ReverseString0Test()
+          {
+               string original = "abcd";
+               string desired = "dcba";
+               string result = Algorithm.ReverseString1(original);
+               Assert.AreEqual(result, desired);
+          }
+
+          [TestMethod]
+          public void ReverseString1Test()
+          {
+               string original = "abcd";
+               string desired = "dcba";
+               string result = Algorithm.ReverseString1(original);
+               Assert.AreEqual(result, desired);
+          }
+
+          [TestMethod]
+          public void ReverseString2Test()
+          {
+               string original = "abcd";
+               string desired = "dcba";
+               string result = Algorithm.ReverseString2(original);
+               Assert.AreEqual(result, desired);
+          }
+
+          [TestMethod]
+          public void FactorialRTest()
+          {
+               int original = 12;
+               int desired = 479001600;
+               DateTime start = DateTime.Now;
+               int result = Algorithm.Factorial(original);
+               DateTime end = DateTime.Now;
+               Debug.WriteLine(end - start);
+               Assert.AreEqual(result, desired);
+          }
+
+          [TestMethod]
+          public void FactorialNRTest()
+          {
+               int original = 12;
+               int desired = 479001600;
+               DateTime start = DateTime.Now;
+               int result = Algorithm.FactorialNonRecur(original);
+               DateTime end = DateTime.Now;
+               Debug.WriteLine(end - start);
+               Assert.AreEqual(result, desired);
+          }
+
+
+          [TestMethod]
+          public void TraverseBTInOrderTest()
+          {
+               BinaryNode<string>[] nodes = new BinaryNode<string>[7];
+               nodes[0] = new BinaryNode<string>("A");
+               nodes[1] = new BinaryNode<string>("B");
+               nodes[2] = new BinaryNode<string>("C");
+               nodes[3] = new BinaryNode<string>("D");
+               nodes[4] = new BinaryNode<string>("E");
+               nodes[5] = new BinaryNode<string>("F");
+               nodes[6] = new BinaryNode<string>("G");
+
+               nodes[3].AddLeftNode(nodes[1]);
+               nodes[3].AddRightNode(nodes[5]);
+               nodes[1].AddLeftNode(nodes[0]);
+               nodes[1].AddRightNode(nodes[2]);
+               nodes[5].AddLeftNode(nodes[4]);
+               nodes[5].AddRightNode(nodes[6]);
+
+               List<string> desired = new List<string> { "A", "B", "C", "D", "E", "F", "G" };
+               List<string> result = new List<string>();
+               Algorithm.TraverseBTInOrder(nodes[3], result);
+               for(int i = 0; i < result.Count; i++)
+               {
+                    Assert.AreEqual(result[i], desired[i]);
+               }
+          }
+
+          [TestMethod]
+          public void TraverseBTPreOrderTest()
+          {
+               BinaryNode<string>[] nodes = new BinaryNode<string>[7];
+               nodes[0] = new BinaryNode<string>("A");
+               nodes[1] = new BinaryNode<string>("B");
+               nodes[2] = new BinaryNode<string>("C");
+               nodes[3] = new BinaryNode<string>("D");
+               nodes[4] = new BinaryNode<string>("E");
+               nodes[5] = new BinaryNode<string>("F");
+               nodes[6] = new BinaryNode<string>("G");
+
+               nodes[3].AddLeftNode(nodes[1]);
+               nodes[3].AddRightNode(nodes[5]);
+               nodes[1].AddLeftNode(nodes[0]);
+               nodes[1].AddRightNode(nodes[2]);
+               nodes[5].AddLeftNode(nodes[4]);
+               nodes[5].AddRightNode(nodes[6]);
+
+               List<string> desired = new List<string> { "D", "B", "A", "C", "F", "E", "G" };
+               List<string> result = new List<string>();
+               Algorithm.TraverseBTPreOrder(nodes[3], result);
+               for (int i = 0; i < result.Count; i++)
+               {
+                    Assert.AreEqual(result[i], desired[i]);
+               }
+          }
+
+          [TestMethod]
+          public void TraverseBTPostOrderTest()
+          {
+               BinaryNode<string>[] nodes = new BinaryNode<string>[7];
+               nodes[0] = new BinaryNode<string>("A");
+               nodes[1] = new BinaryNode<string>("B");
+               nodes[2] = new BinaryNode<string>("C");
+               nodes[3] = new BinaryNode<string>("D");
+               nodes[4] = new BinaryNode<string>("E");
+               nodes[5] = new BinaryNode<string>("F");
+               nodes[6] = new BinaryNode<string>("G");
+
+               nodes[3].AddLeftNode(nodes[1]);
+               nodes[3].AddRightNode(nodes[5]);
+               nodes[1].AddLeftNode(nodes[0]);
+               nodes[1].AddRightNode(nodes[2]);
+               nodes[5].AddLeftNode(nodes[4]);
+               nodes[5].AddRightNode(nodes[6]);
+
+               List<string> desired = new List<string> { "A", "C", "B", "E", "G", "F", "D" };
+               List<string> result = new List<string>();
+               Algorithm.TraverseBTPostOrder(nodes[3], result);
+               for (int i = 0; i < result.Count; i++)
+               {
+                    Assert.AreEqual(result[i], desired[i]);
+               }
+          }
+
+          [TestMethod]
+          public void QuestionMarkTest()
+          {
+               string s1 = "arrb6???4xxbl5???eee5";
+               bool result1 = Algorithm.QuestionMark(s1);
+               string s2 = "acc?7??sss?3rr1??????5";
+               bool result2 = Algorithm.QuestionMark(s2);
+               string s3 = "5??aaaaaaaaaaaaaaaaaaa?5?5";
+               bool result3 = Algorithm.QuestionMark(s3);
+               string s4 = "9???1???9???1???9";
+               bool result4 = Algorithm.QuestionMark(s4);
+               string s5 = "aa6?4";
+               bool result5 = Algorithm.QuestionMark(s5);
+               Debug.WriteLine(result1.ToString() + result2.ToString() + result3.ToString() + result4.ToString() + result5.ToString());
+               Assert.IsTrue(result1);
+               Assert.IsTrue(result2);
+               Assert.IsFalse(result3);
+               Assert.IsTrue(result4);
+               Assert.IsFalse(result5);
+
+          }
+
+          [TestMethod]
+          public void BalanceScaleTest()
+          {
+               string[] input1 = { "[3, 4]", "[1, 2, 7, 7]" };
+               string result1 = Algorithm.BalanceScale(input1);
+               string[] input2 = { "[13, 4]", "[1, 2, 3, 6, 14]" };
+               string result2 = Algorithm.BalanceScale(input2);
+               string[] input3 = { "[13, 4]", "[1, 2, 10, 11, 14]" };
+               string result3 = Algorithm.BalanceScale(input3);
+               Debug.WriteLine("------------------");
+               Debug.WriteLine(result1);
+               Debug.WriteLine(result2);
+               Debug.WriteLine(result3);
+               Debug.WriteLine("------------------");
+               Assert.AreEqual(result1, "1");
+               Assert.AreEqual(result2, "3,6");
+               Assert.AreEqual(result3, "not possible");
+          }
+
+     }
+}
